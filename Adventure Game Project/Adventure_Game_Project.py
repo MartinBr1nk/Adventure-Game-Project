2a8cafe013b395ffc6a9675bfcfb236b72b17230
JackOSchool <jack.oreilly@bcestudent.com>
2024-09-12 14:11:19 +0100

    Attempted save system
    didnt work :(

--- Adventure Game Project/Adventure_Game_Project.py
+++ Adventure Game Project/Adventure_Game_Project.py
@@ -1,24 +1,40 @@
 #Imports
 import time
 import random
+from SaveData import *
 
 #functions
+def Save(ValueName, Value):
+    f = open("SaveData.py", "a")
+    f.write(str(ValueName) + " = " + str(Value) + "\n")
+    f.close()
+
+RandomVal = random.randint(0, 500)
+print(RandomVal)
+#Random Value generated at the start of every run that can cause special events to occour
 
 
-#Save data system here
-
-
-
-PlayerName = input("Name the protagonist: ")
-
-if PlayerName == "":
-    PlayerName = input("Enter a name, otherwise a default name will be chosen: ")
+if RanPreviously == False:
+    PlayerName = input("Name the protagonist: ")
     if PlayerName == "":
-        PlayerName = "Stanley"
-        #this is a placeholder name, I'm not actually going to steal EVERY idea from the stanley parable
-        print(f"Your name is {PlayerName}.")
+
+        PlayerName = input("Enter a name, otherwise a default name will be chosen: ")
+        if PlayerName == "":
+
+            PlayerName = "Stanley"
+            #this is a placeholder name, I'm not actually going to steal EVERY idea from the stanley parable
+            print(f"Your name is {PlayerName}.")
+            #Save("RanPreviously", "True")
+            #Save("Name", PlayerName)
+
+        else:
+            print(f"Your name is {PlayerName}.")
+            #Save("RanPreviously", "True")
+            #Save("Name", PlayerName)
+
     else:
         print(f"Your name is {PlayerName}.")
-else:
-    print(f"Your name is {PlayerName}.")
+        #Save("RanPreviously", "True")
+        #Save("Name", PlayerName)
 
+        
---
+++ Adventure Game Project/SaveData.py
@@ -0,0 +1,2 @@
+RanPreviously = False
+
