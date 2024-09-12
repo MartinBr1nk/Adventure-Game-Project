cf6eb9ea202be5f9c8d1e7995a6bd5494addecb8
JackOSchool <jack.oreilly@bcestudent.com>
2024-09-12 11:08:39 +0100

    omg game starting
    crazy!!

--- Adventure Game Project/Adventure_Game_Project.py
+++ Adventure Game Project/Adventure_Game_Project.py
@@ -1,3 +1,12 @@
-print("game of the year 2069")
-print("this is a test to see if I can update github :O")
-#comment!!!
+PlayerName = input("Name the protagonist: ")
+
+if PlayerName == "":
+    PlayerName = input("Enter a name, otherwise a default name will be chosen: ")
+    if PlayerName == "":
+        PlayerName = "Stanley"
+        #this is a placeholder name, I'm not actually going to steal EVERY idea from the stanley parable
+        print(f"Your name is {PlayerName}.")
+    else:
+        print(f"Your name is {PlayerName}.")
+else:
+    print(f"Your name is {PlayerName}.")
