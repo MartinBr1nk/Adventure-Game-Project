bdb2f699298d603f05d07cf8c689971d29d46cf1
JackOReillyBigCreative <jack.oreilly@bcestudent.com>
2024-09-18 11:28:38 +0100

    pep 8 2
    fixed an INCREDIBLY LONG line of code to make it compliant with Pep 8 conventions.

--- Adventure Game Project/Introduction.py
+++ Adventure Game Project/Introduction.py
@@ -46,7 +46,14 @@ def IntroSequence():
     print("PLEASE ENTER A NAME SO YOUR CADEVER CAN BE IDENTIFIED")
     time.sleep(2)
     player_name = input("PILOT NAME: ")
-    if "skibidi" in player_name.lower() or "gyatt" in player_name.lower() or "rizz" in player_name.lower() or "sigma" in player_name.lower() or "ohio" in player_name.lower() or "hawk" in player_name.lower() or "tuah" in player_name.lower() or "aura" in player_name.lower() or "fanum" in player_name.lower() or "alpha" in player_name.lower() or "mew" in player_name.lower() or "mog" in player_name.lower() or "gronk" in player_name.lower() or "glazing" in player_name.lower() or "cenat" in player_name.lower():
+    if ["skibidi" in player_name.lower() or "gyatt" in player_name.lower()
+       or "rizz" in player_name.lower() or "sigma" in player_name.lower()
+      or "ohio" in player_name.lower() or "hawk" in player_name.lower()
+     or "tuah" in player_name.lower() or "aura" in player_name.lower()
+    or "fanum" in player_name.lower() or "alpha" in player_name.lower()
+   or "mew" in player_name.lower() or "mog" in player_name.lower()
+  or "gronk" in player_name.lower() or "glazing" in player_name.lower()
+ or "cenat" in player_name.lower()]:
         #if these terms are within your name, the game just kills you before you can start.
         cool_typing("RELEASING TOXIC GAS...")
         time.sleep(3)
